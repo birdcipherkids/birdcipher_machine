beefddd3adcd5ca052aea7cb81f880efebc9a4a9
Geovanny <birdcode.py@gmail.com>
2024-11-16 13:14:07 -0500

    Adjustments of password checking logos part 2

--- BirdCipher_Machine.py
+++ BirdCipher_Machine.py
@@ -863,13 +863,18 @@ bc_logo_login.place(x = 420, y = 30)
 
 password_checking_logo = tk.PhotoImage(file = 'Password checking-logo-white1.png')
 hibp1_logo = tk.PhotoImage(file = 'hibp1.png')
+hibp_info_logo = tk.PhotoImage(file = 'Password Check Info-logo-white1.png')
 
 password_checking_button = tk.Button(passcheck, image = password_checking_logo)
-password_checking_button.config(bg = '#260223')
+password_checking_button.config(bg = '#067297')
 password_checking_button.place(x = 610, y = 20)
 
 hibp_logo = tk.Label(passcheck, image = hibp1_logo)
 hibp_logo.place(x = 610, y = 400)
+
+hibp_info = tk.Button(passcheck, image = hibp_info_logo)
+hibp_info.config(bg = '#067297')
+hibp_info.place(x = 920, y = 401)
 
 
 cipher_text = tk.Text(fr, font = ("Comic Sans MS", 9))
