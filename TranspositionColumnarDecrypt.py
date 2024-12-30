4dd479dd8cb85871b10932cab8ef16830ba73dfe
Geovanny <birdcode.py@gmail.com>
2024-12-30 11:31:50 -0500

    Desarrollo de algoritmo de descifrado por transposicion lineal - avance

--- TranspositionColumnarDecrypt.py
+++ TranspositionColumnarDecrypt.py
@@ -8,51 +8,93 @@ def decryptMessagesInverse(key, message):
 
 	numberOfColumns = math.ceil(len(message)/key)
 	numberOfRows = key
+	grid = numberOfColumns * numberOfRows
 	numberShadedBoxes = (numberOfColumns * numberOfRows) - len(message)
+	Fsb = numberOfRows - numberShadedBoxes
 
 	plaintext = [''] * numberOfColumns
 
 	
-	
-
 	for col in range(numberOfColumns):
 
-		if col % 2 != 0:
+		countCaracters = 0
+
+		if col % 2 != 0 or col % 2 == 0:
 
 			pointer = col
 			row = 0
 
 			while row < numberOfRows:
 
-				plaintext[col] = plaintext[col] + message[pointer]
-				pointer = pointer + numberOfColumns
-				row = row + 1
+				if pointer < len(message) and col <= numberOfColumns - 1: 
 
-		
-		elif col % 2 == 0:
+					if row < Fsb and col < numberOfColumns - 1:
 
-			pointer = col
-			pointer = pointer + (numberOfColumns * (key - 1))
-			row = numberOfRows
+						plaintext[col] = plaintext[col] + message[pointer]
+						pointer = pointer + numberOfColumns
+						row = row + 1
+					
+					elif row >= Fsb and col < numberOfColumns - 1:
 
-			while row > 0:
+						plaintext[col] = plaintext[col] + message[pointer]
+						pointer = pointer + numberOfColumns - 1
+						row = row + 1
 
-				plaintext[col] = plaintext[col] + message[pointer]
-				pointer = pointer - numberOfColumns
-				row = row - 1
+					elif row <= Fsb - 1 and col == numberOfColumns - 1:
 
-			
+						plaintext[col] = plaintext[col] + message[pointer]
+						pointer = pointer + numberOfColumns
+						row = row + 1
+
+					elif row > Fsb - 1 and col == numberOfColumns - 1:
+
+						break
+
+				
+				
+
+					
+
+				
+					
+				
+
+
+
+				
+
+		# elif col % 2 == 0:
+
+		# 	pointer = col
+		# 	pointer = pointer + (numberOfColumns * (key - 1))
+		# 	row = numberOfRows
+
+		# 	# elif numberShadedBoxes > 0:
+
+		# 	# 	pointer = col
+		# 	# 	pointer = pointer + (numberOfColumns * (key - 1)) - numberShadedBoxes
+
+		# 	while row > 0:
+
+		# 		if pointer >= col and countCaracters <= len(message):
+
+		# 			plaintext[col] = plaintext[col] + message[pointer]
+		# 			pointer = pointer - numberOfColumns
+		# 			row = row - 1
+		# 			countCaracters = countCaracters + 1
+
+		# 		else:
+		# 			break
+
+	print(numberShadedBoxes)
+	print(plaintext)		
 	return ''.join(plaintext)
+
+
 
 print(decryptMessagesInverse(keys, messag))
 
 
 
-
-
-
-
-
-			
 
 
