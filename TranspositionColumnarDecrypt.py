ebc406e7ffc1071ccf10c2ee3e3aa7de9f47ef93
Geovanny <birdcode.py@gmail.com>
2025-01-03 13:01:37 -0500

    Ajuste algoritmo de cifrado por transposicion columnar descendente ascendente

--- TranspositionColumnarDecrypt.py
+++ TranspositionColumnarDecrypt.py
@@ -14,77 +14,76 @@ def decryptMessagesInverse(key, message):
 
 	plaintext = [''] * numberOfColumns
 
-	
 	for col in range(numberOfColumns):
 
 		countCaracters = 0
 
-		if col % 2 != 0 or col % 2 == 0:
+		if col % 2 != 0:
 
 			pointer = col
-			row = 0
+			row = 1
 
-			while row < numberOfRows:
+			while row <= numberOfRows:
 
 				if pointer < len(message) and col <= numberOfColumns - 1: 
 
-					if row < Fsb and col < numberOfColumns - 1:
+					if row <= numberShadedBoxes and col < numberOfColumns - 1:
+
+						plaintext[col] = plaintext[col] + message[pointer]
+						pointer = pointer + numberOfColumns - 1
+						row = row + 1
+					
+					elif row > numberShadedBoxes and col < numberOfColumns - 1:
 
 						plaintext[col] = plaintext[col] + message[pointer]
 						pointer = pointer + numberOfColumns
 						row = row + 1
-					
-					elif row >= Fsb and col < numberOfColumns - 1:
+
+					elif row <= numberShadedBoxes and col == numberOfColumns - 1:
 
 						plaintext[col] = plaintext[col] + message[pointer]
 						pointer = pointer + numberOfColumns - 1
 						row = row + 1
 
-					elif row <= Fsb - 1 and col == numberOfColumns - 1:
-
-						plaintext[col] = plaintext[col] + message[pointer]
-						pointer = pointer + numberOfColumns
-						row = row + 1
-
-					elif row > Fsb - 1 and col == numberOfColumns - 1:
+					elif row > numberShadedBoxes and col == numberOfColumns - 1:
 
 						break
 
-				
-				
+		
+		elif col % 2 == 0:
+
+			pointer = col
+			pointer = pointer + (numberOfColumns * (key - 1)) - numberShadedBoxes
+			row = numberOfRows
+
+			
+			while row > 0:
+
+				if pointer < len(message) and col <= numberOfColumns - 1:
+
+					if row > numberShadedBoxes + 1 and col < numberOfColumns - 1:
+
+						plaintext[col] = plaintext[col] + message[pointer]
+						pointer = pointer - numberOfColumns
+						row = row - 1
+
+					elif row <= numberShadedBoxes + 1 and col < numberOfColumns - 1:
+
+						plaintext[col] = plaintext[col] + message[pointer]
+						pointer = pointer - (numberOfColumns - 1)
+						row = row - 1
+
+					elif row > numberShadedBoxes and col == numberOfColumns - 1:
+
+						plaintext[col] = plaintext[col] + message[pointer]
+						pointer = pointer - numberOfColumns
+						row = row - 1
+
+					elif row == numberShadedBoxes and col == numberOfColumns - 1:
+
+						break
 
 					
-
-				
-					
-				
-
-
-
-				
-
-		# elif col % 2 == 0:
-
-		# 	pointer = col
-		# 	pointer = pointer + (numberOfColumns * (key - 1))
-		# 	row = numberOfRows
-
-		# 	# elif numberShadedBoxes > 0:
-
-		# 	# 	pointer = col
-		# 	# 	pointer = pointer + (numberOfColumns * (key - 1)) - numberShadedBoxes
-
-		# 	while row > 0:
-
-		# 		if pointer >= col and countCaracters <= len(message):
-
-		# 			plaintext[col] = plaintext[col] + message[pointer]
-		# 			pointer = pointer - numberOfColumns
-		# 			row = row - 1
-		# 			countCaracters = countCaracters + 1
-
-		# 		else:
-		# 			break
 
 	print(numberShadedBoxes)
 	print(plaintext)		
