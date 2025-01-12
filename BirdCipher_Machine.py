a577860a669d69b10fa4e3d8ef03b51f51c286ed
BirdCode888 <birdcode.cali@gmail.com>
2025-01-12 18:15:56 -0500

    Desarrollo de seccion de virus total parte 1

--- BirdCipher_Machine.py
+++ BirdCipher_Machine.py
@@ -32,6 +32,7 @@ lives = 5
 counter_social_eng = -1
 directory = ''
 directoryHash = ''
+directoryVirusTotal = ''
 username_db = ''
 key_ramson = ''
 login_check = False
@@ -136,6 +137,13 @@ def selectDirectoryHash():
 
 	directoryHash = filedialog.askopenfilename(title = 'Open file to hash')
 	archiveURLShow.config(text = archive_url.set(directoryHash))
+
+def selectDirectoryVirusTotal():
+
+	global directoryVirusTotal
+
+	directoryVirusTotal = filedialog.askopenfilename(title = 'Open file to upload in Virus Total')
+	urlUploadFile.config(text = archive_upload_vt.set(directoryVirusTotal))
 
 def generate_key_ramson():
 
@@ -846,6 +854,7 @@ person4c_activated = False
 encrypt_buttonImg = tk.PhotoImage(file = "Encrypt-logo1.png")
 decrypt_buttonImg = tk.PhotoImage(file = "Decrypt-logo1.png")
 directory_browser = tk.PhotoImage(file = 'Browse directories.png')
+directory_browser1 = tk.PhotoImage(file = 'Browse directories1.png')
 ramson_instructions = tk.PhotoImage(file = 'Instructions.png')
 generateRamsonKey_de = tk.PhotoImage(file = 'Generate RamsonBird Key.png')
 bringRamsonKey_de = tk.PhotoImage(file = 'Bring RamsonBird key.png')
@@ -1279,6 +1288,71 @@ closeBCM_hashing = tk.Button(hashing, image = closeLog, command = lambda:closeMa
 closeBCM_hashing.place(x = 950 , y = 10)
 
 # -------------------------------------------------------------------------------------------------------------------------
+
+
+### ---------------------------------------------- Virus Total section -----------------------------------------------------
+
+archive_upload_vt = tk.StringVar()
+formatUploadFile = tk.IntVar()
+
+
+titleVirusTotal = tk.Label(virusTotal, text = 'Upload your file to Virus Total')
+titleVirusTotal.config(font = ('Comic Sans MS', 13), fg = '#7a0684')
+titleVirusTotal.place(x = 50, y = 20)
+
+urlUploadFile = tk.Entry(virusTotal, textvariable = archive_upload_vt, font = ('Comic Sans MS', 8), width = 65)
+urlUploadFile.config(bg = '#050005', fg = '#f7a6f1', justify = 'center')
+urlUploadFile.place(x = 20, y = 60)
+
+urlUploadLogo = tk.Button(virusTotal, image = directory_browser1, command = lambda:selectDirectoryVirusTotal())
+urlUploadLogo.place(x = 500, y = 45)
+
+formatLabel = tk.Label(virusTotal, text = 'File format')
+formatLabel.config(font = ('Comic Sans MS', 11), fg = '#7a0684')
+formatLabel.place(x = 30, y = 120)
+
+jpg_button = tk.Radiobutton(virusTotal, text = 'JPG', variable = formatUploadFile, value = 0)
+jpg_button.config(font = ('Comic Sans MS', 10), justify = 'left', fg = '#7a0684')
+jpg_button.place(x = 30, y = 160)
+
+png_button = tk.Radiobutton(virusTotal, text = 'PNG', variable = formatUploadFile, value = 1)
+png_button.config(font = ('Comic Sans MS', 10), justify = 'left', fg = '#7a0684')
+png_button.place(x = 100, y = 160)
+
+word_button = tk.Radiobutton(virusTotal, text = 'Word', variable = formatUploadFile, value = 2)
+word_button.config(font = ('Comic Sans MS', 10), justify = 'left', fg = '#7a0684')
+word_button.place(x = 170, y = 160)
+
+excel_button = tk.Radiobutton(virusTotal, text = 'Excel', variable = formatUploadFile, value = 3)
+excel_button.config(font = ('Comic Sans MS', 10), justify = 'left', fg = '#7a0684')
+excel_button.place(x = 240, y = 160)
+
+pp_button = tk.Radiobutton(virusTotal, text = 'Power Point', variable = formatUploadFile, value = 4)
+pp_button.config(font = ('Comic Sans MS', 10), justify = 'left', fg = '#7a0684')
+pp_button.place(x = 310, y = 160)
+
+pdf_button = tk.Radiobutton(virusTotal, text = 'PDF', variable = formatUploadFile, value = 5)
+pdf_button.config(font = ('Comic Sans MS', 10), justify = 'left', fg = '#7a0684')
+pdf_button.place(x = 420, y = 160)
+
+mp3_button = tk.Radiobutton(virusTotal, text = 'mp3', variable = formatUploadFile, value = 6)
+mp3_button.config(font = ('Comic Sans MS', 10), justify = 'left', fg = '#7a0684')
+mp3_button.place(x = 485, y = 160)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# --------------------------------------------------------------------------------------------------------------------------
 
 
 
